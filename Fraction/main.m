800c430955d922c18c32b519e13fbbe71733cb90
hello <parmjit.khattra@gmail.com>
2014-12-02 00:14:48 +0530

    first changes

--- Fraction/main.m
+++ Fraction/main.m
@@ -1,17 +1,43 @@
-//
-//  main.m
-//  Fraction
-//
-//  Created by KHATTRA on 01/12/14.
-//  Copyright (c) 2014 xenon. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+//--------@interface section--------
+@interface Fraction: NSObject
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+@end
+//---------@implementaion section--------
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+-(void) print
+{
+    NSLog(@"%i/%i", numerator,denominator);
+}
+-(void) setNumerator: (int) n
+{
+    numerator=n;
+}
+-(void) setDenominator: (int) d
+{
+    denominator=d;
+}
+@end
+//------programming section----------
+int main(int args, const char * argv[])
+{
+    @autoreleasepool{
+        Fraction *myFraction1=[[Fraction alloc] init ];
+        Fraction *myFraction2=[[Fraction alloc] init ];
+        [myFraction1 setNumerator:8];
+        [myFraction1 setDenominator:4];
+        [myFraction2 setNumerator:10];
+        [myFraction2 setDenominator:5];
+        NSLog(@"the first fraction result is:");
+        [myFraction1 print];
+        NSLog(@"the second fraction result is:");
+        [myFraction2 print];
     }
     return 0;
 }
