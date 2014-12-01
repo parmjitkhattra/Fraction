48a8b626991babc53d8d01810b83d0ec8a86cf63
hello <parmjit.khattra@gmail.com>
2014-12-01 20:49:00 +0530

    Initial Commit

---
+++ Fraction/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fraction
+//
+//  Created by KHATTRA on 01/12/14.
+//  Copyright (c) 2014 xenon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
