0c01f5604bea340a8bb37647af4bf89f9b4dcbe2
hello <parmjit.khattra@gmail.com>
2014-12-02 02:08:42 +0530

    add addition method
    it is working now

--- Fraction/main.m
+++ Fraction/main.m
@@ -1,17 +1,19 @@
 //Here i would like to know the diff between instace method & class method
+//
 
 #import <Foundation/Foundation.h>
 //--------@interface section--------
 @interface Fraction: NSObject
+-(void) print: (int) result;
 -(void) print;
 -(void) setNumerator: (int) n;
 -(void) setDenominator: (int) d;
+-(int) addition;
 @end
 //---------@implementaion section--------
 @implementation Fraction
 {
-    int numerator;
-    int denominator;
+    int numerator,denominator,result,total;
 }
 -(void) print
 {
@@ -25,6 +27,17 @@
 {
     denominator=d;
 }
+-(int) addition;
+{
+    total=numerator+denominator;
+    return total;
+}
+-(void) print: (int) r
+{
+    result=r;
+    NSLog(@"the total is %i", result);
+}
+
 @end
 //------programming section----------
 int main(int args, const char * argv[])
@@ -36,6 +49,8 @@ int main(int args, const char * argv[])
         [myFraction1 setDenominator:2];
         [myFraction2 setNumerator:6];
         [myFraction2 setDenominator:2];
+        int r=[myFraction1 addition];
+        [myFraction1 print:r];
         NSLog(@"the first fraction result is:");
         [myFraction1 print];
         NSLog(@"the second fraction result is:");
